b3b15b3e7853ca0baa7a6c50284bce1fa488f768
Stephan <stephan-zager@web.de>
2025-08-11 15:34:55 +0200

    Profile model

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    file = models.ImageField(upload_to='profile_pics/', null=True, blank=True) 
+    location = models.CharField(max_length=25, default='')
+    tel = models.CharField(max_length=15, default='')
+    description= models.TextField(max_length=255, default='')
+    working_hours= models.CharField(max_length=20, default='')
