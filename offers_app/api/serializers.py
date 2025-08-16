d9f726e5d6674828598159925e51ae1a30ea572d
Stephan <stephan-zager@web.de>
2025-08-16 16:41:10 +0200

    serach and filter deleted again. I want to go a new way.

--- offers_app/api/serializers.py
+++ offers_app/api/serializers.py
@@ -1,28 +1,31 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from user_profile.models import Profile
-from rest_framework import serializers
 from ..models import Offer, OfferDetail
 from django.db.models import Min
 
+
 class OfferDetailListSerializer(serializers.ModelSerializer):
     url = serializers.SerializerMethodField()
+
     class Meta:
         model = OfferDetail
         fields = ['id', 'url']
+
     def get_url(self, obj):
         return f"http://127.0.0.1:8000/api/offerdetails/{obj.id}/"
+
 
 class UserDetailsSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username']
 
+
 class OfferListSerializer(serializers.ModelSerializer):
     details = OfferDetailListSerializer(many=True, read_only=True)
-    min_price = serializers.SerializerMethodField()
-    min_delivery_time = serializers.SerializerMethodField()
     user_details = UserDetailsSerializer(source='user', read_only=True)
+    created_at = serializers.DateTimeField(read_only=True)
+    updated_at = serializers.DateTimeField(read_only=True)
 
     class Meta:
         model = Offer
@@ -35,18 +38,9 @@ class OfferListSerializer(serializers.ModelSerializer):
             'created_at',
             'updated_at',
             'details',
-            'min_price',
-            'min_delivery_time',
             'user_details',
         ]
 
-    def get_min_price(self, obj):
-        details = obj.details.all()
-        return details.aggregate(Min('price'))['price__min'] if details else None
-
-    def get_min_delivery_time(self, obj):
-        details = obj.details.all()
-        return details.aggregate(Min('delivery_time_in_days'))['delivery_time_in_days__min'] if details else None
 
 class OfferDetailSerializer(serializers.ModelSerializer):
     class Meta:
@@ -61,10 +55,12 @@ class OfferDetailSerializer(serializers.ModelSerializer):
             'offer_type',
         ]
 
+
 class OfferRetrieveSerializer(serializers.ModelSerializer):
     details = OfferDetailListSerializer(many=True, read_only=True)
-    min_price = serializers.SerializerMethodField()
-    min_delivery_time = serializers.SerializerMethodField()
+    created_at = serializers.DateTimeField(read_only=True)
+    updated_at = serializers.DateTimeField(read_only=True)
+
     class Meta:
         model = Offer
         fields = [
@@ -76,18 +72,12 @@ class OfferRetrieveSerializer(serializers.ModelSerializer):
             'created_at',
             'updated_at',
             'details',
-            'min_price',
-            'min_delivery_time',
         ]
-    def get_min_price(self, obj):
-        details = obj.details.all()
-        return details.aggregate(Min('price'))['price__min'] if details else None
-    def get_min_delivery_time(self, obj):
-        details = obj.details.all()
-        return details.aggregate(Min('delivery_time_in_days'))['delivery_time_in_days__min'] if details else None
+
 
 class OfferUpdateSerializer(serializers.ModelSerializer):
     details = OfferDetailSerializer(many=True, required=False)
+
     class Meta:
         model = Offer
         fields = [
@@ -97,20 +87,7 @@ class OfferUpdateSerializer(serializers.ModelSerializer):
             'description',
             'details',
         ]
-    def update(self, instance, validated_data):
-        details_data = validated_data.pop('details', None)
-        for attr, value in validated_data.items():
-            setattr(instance, attr, value)
-        instance.save()
-        if details_data:
-            for detail_data in details_data:
-                offer_type = detail_data.get('offer_type')
-                detail_obj = instance.details.filter(offer_type=offer_type).first()
-                if detail_obj:
-                    for key, val in detail_data.items():
-                        setattr(detail_obj, key, val)
-                    detail_obj.save()
-        return instance
+
 
 class OfferSerializer(serializers.ModelSerializer):
     details = OfferDetailSerializer(many=True, read_only=True)
@@ -126,5 +103,17 @@ class OfferSerializer(serializers.ModelSerializer):
             'details',
         ]
 
-    def create(self, validated_data):
-        return Offer.objects.create(**validated_data)
+
+class OfferSerializer(serializers.ModelSerializer):
+    details = OfferDetailSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Offer
+        fields = [
+            'id',
+            'user',
+            'title',
+            'image',
+            'description',
+            'details',
+        ]
--- offers_app/api/views.py
+++ offers_app/api/views.py
@@ -1,26 +1,23 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import generics, filters, status
+from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
-from django.db.models import Min
 from .serializers import (
     OfferDetailSerializer, OfferSerializer, OfferListSerializer,
     OfferRetrieveSerializer, OfferUpdateSerializer
 )
 from ..models import Offer, OfferDetail
-from django.contrib.auth.models import User
 from rest_framework.permissions import IsAuthenticated
 from .permissions import IsBusinessUser, IsOwner
+
 
 class OfferPagination(PageNumberPagination):
     page_size_query_param = 'page_size'
 
+
 class OfferView(generics.ListCreateAPIView):
     queryset = Offer.objects.all()
     pagination_class = OfferPagination
-    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
-    search_fields = ['title', 'description']
-    ordering_fields = ['updated_at', 'min_price']
 
     def get_permissions(self):
         if self.request.method == 'POST':
@@ -31,23 +28,6 @@ class OfferView(generics.ListCreateAPIView):
         if self.request.method == 'GET':
             return OfferListSerializer
         return OfferSerializer
-
-    def get_queryset(self):
-        qs = Offer.objects.all()
-        creator_id = self.request.query_params.get('creator_id')
-        min_price = self.request.query_params.get('min_price')
-        max_delivery_time = self.request.query_params.get('max_delivery_time')
-        ordering = self.request.query_params.get('ordering')
-
-        if creator_id:
-            qs = qs.filter(user_id=creator_id)
-        if min_price:
-            qs = qs.filter(details__price__gte=min_price)
-        if max_delivery_time:
-            qs = qs.filter(details__delivery_time_in_days__lte=max_delivery_time)
-        if ordering:
-            qs = qs.order_by(ordering)
-        return qs.distinct()
 
     def create(self, request, *args, **kwargs):
         data = request.data
@@ -64,37 +44,9 @@ class OfferView(generics.ListCreateAPIView):
 
     def list(self, request, *args, **kwargs):
         queryset = self.get_queryset()
-        page = self.paginate_queryset(queryset)
-        results = []
-        offers = page if page is not None else queryset
-        for offer in offers:
-            details = offer.details.all()
-            min_price = details.aggregate(Min('price'))['price__min'] if details else None
-            min_delivery_time = details.aggregate(Min('delivery_time_in_days'))['delivery_time_in_days__min'] if details else None
-            user = offer.user if hasattr(offer, 'user') and offer.user is not None else None
-            user_details = {
-                "first_name": user.first_name if user else "",
-                "last_name": user.last_name if user else "",
-                "username": user.username if user else "",
-            }
-            created_at = offer.created_at if hasattr(offer, 'created_at') else None
-            updated_at = offer.updated_at if hasattr(offer, 'updated_at') else None
-            results.append({
-                "id": offer.id,
-                "user": user.id if user else None,
-                "title": offer.title,
-                "image": offer.image.url if offer.image else None,
-                "description": offer.description,
-                "created_at": created_at,
-                "updated_at": updated_at,
-                "details": [{"id": d.id, "url": f"/offerdetails/{d.id}/"} for d in details],
-                "min_price": min_price,
-                "min_delivery_time": min_delivery_time,
-                "user_details": user_details,
-            })
-        if page is not None:
-            return self.get_paginated_response(results)
-        return Response(results)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
 
 class OfferDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Offer.objects.all()
@@ -106,6 +58,7 @@ class OfferDetailView(generics.RetrieveUpdateDestroyAPIView):
         if self.request.method in ['PATCH', 'PUT']:
             return OfferUpdateSerializer
         return OfferRetrieveSerializer
+
 
 class OfferDetailObjView(generics.RetrieveAPIView):
     queryset = OfferDetail.objects.all()
