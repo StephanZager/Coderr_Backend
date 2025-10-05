02518f43c1466ae98b31846173f09e0d7b85e3ed
Stephan <stephan-zager@web.de>
2025-10-05 11:18:48 +0200

    login with username

--- user_auth_app/api/serializers.py
+++ user_auth_app/api/serializers.py
@@ -29,56 +29,61 @@ class RegistrationSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError(
                 {'error': 'A user with this email already exists'}
             )
+        # Prüfe ob Username bereits existiert
+        if User.objects.filter(username=data['username']).exists():
+            raise serializers.ValidationError(
+                {'error': 'A user with this username already exists'}
+            )
         return data
 
     def create(self, validated_data):
         """
-        Create and return a new User instance, after removing the repeated_password and type fields.
-        Uses email as username (which is Django-compliant) and stores the full name in first_name and last_name fields.
+        Create and return a new User instance, keeping the original username.
+        Stores the username as-is and extracts first_name/last_name from it.
         """
         validated_data.pop('repeated_password')
         user_type = validated_data.pop('type')  
         
-        full_name = validated_data.pop('username')  
-        email = validated_data['email']
-        validated_data['username'] = email
+        # Behalte den originalen Username
+        original_username = validated_data['username']
+        
+        # Erstelle User mit dem originalen Username
         user = User.objects.create_user(**validated_data)
         
         Profile.objects.create(user=user, type=user_type)
 
-        if full_name:
+        # Extrahiere Namen aus dem Username für first_name/last_name
+        if original_username:
             try:
-                first_name, last_name = full_name.split(' ', 1)
+                first_name, last_name = original_username.split(' ', 1)
                 user.first_name = first_name.capitalize()
                 user.last_name = last_name.capitalize()
             except ValueError:
-                user.first_name = full_name.capitalize()
+                user.first_name = original_username.capitalize()
                 user.last_name = ''
             
             user.save(update_fields=['first_name', 'last_name'])
-        user.full_name = full_name
-        return user
         
+        return user
+
 class EmailAuthTokenSerializer(serializers.Serializer):
-    email = serializers.EmailField()
+    username = serializers.CharField()
     password = serializers.CharField(write_only=True)
 
     def validate(self, data):
         """
-        Validate the provided email and password.
-        Checks if a user with the given email exists and if the password is correct.
-        If valid, adds the user instance to the validated data.
+        Validate the provided username and password.
         """
-        email = data.get('email')
+        username = data.get('username')
         password = data.get('password')
 
         try:
-            user = User.objects.get(email=email)
+            user = User.objects.get(username=username)
         except User.DoesNotExist:
             raise serializers.ValidationError(
-                "User with this email does not exist")
+                "User with this username does not exist")
             
-        user = authenticate(username=email, password=password)
+        user = authenticate(username=username, password=password)
 
         if not user:
             raise serializers.ValidationError("Invalid login credentials.")
